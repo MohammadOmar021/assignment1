d977ad32b272b330c69235d2744b78956f8c568c
mohammadomar <umerbhatti021@gmail.com>
2024-12-17 15:15:19 +0500

    completed

---
+++ app.js
@@ -0,0 +1,104 @@
+// Chapter 1
+
+//Question 1
+
+//alert("Hello, Wlecome to my Website");
+
+// Question 2
+
+// alert("Errpr! Please enter a valid password")
+
+// Question 3
+
+// alert("Welcome to JS Land \n Happy Coding!")
+
+//Question 4
+
+// alert("Welcome to JS Land");
+// alert("Happy Coding! \nPrevent this page from creating dialogs");
+
+// Chapter 2
+//Question 1
+
+// let username;
+
+//Question 2
+
+// let myName = "Muhammad Umer Saeed";
+
+//Question 3
+
+// let message;
+// message = "Hello World";
+// alert(message);
+
+//Question 4
+
+// let name="Jhone Doe";
+// let age="18 years old";
+// let course="Certified Mobile app developer";
+
+// alert(name);
+// alert(age);
+// alert(course);
+
+//Question5
+
+// let pizza="PIZZA";
+
+// alert(pizza);
+// alert(pizza.slice(0,4));
+// alert(pizza.slice(0,3));
+// alert(pizza.slice(0,2));
+// alert(pizza.slice(0,1));
+
+//Question 6
+
+// let email="umerbhatti021@gmail.com";
+
+// alert("My email address is"+email);
+
+//Question 7
+
+// let book = "A smarter way to learn JavaScript";
+
+// alert("I am trying to learn from the Book \n"+book)
+
+//Question8
+
+// document.write("<h1>Hey I can write html through javascript</h1>")
+
+//Question9
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+//Chapter 3
+
+//Question1
+
+// let age=22;
+
+// alert("My age is "+age)
+
+//Question2
+
+// let visited = 15;
+
+// alert("You have visited this website "+ visited+ " times")
+
+//Question3
+
+// let birthyear = 2002;
+
+// document.write("My Birth year is "+birthyear+"<br> Data Type of my declared variable is number");
+
+//QUestion4
+
+// let visitorName="John Doe";
+// let productTitle="tshirt";
+// let quantity=5;
+// let storenName="xyzClothing"
+
+// alert(visitorName+" Ordered "+quantity+" "+productTitle+" from "+storenName);
+
+// document.write(visitorName+" Ordered "+quantity+" "+productTitle+" from "+storenName)
